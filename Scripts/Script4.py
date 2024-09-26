9e42af60fbbfa0b2c2625cfc5c13ecb4a2b4666b
Giovanni Benedet Rodrigues <gi-benedet@hotmail.com>
2024-09-26 20:59:18 -0300

    EX_8_parte_4
    Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.

---
+++ Scripts/Script4.py
@@ -0,0 +1,16 @@
+#Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+
+from datetime import date
+
+nasc = int(input('digite seu ano de nascimento: '))
+
+idade = date.today().year - nasc
+
+if idade == 18 or idade == 17:
+    print('esta na hora de se alistar.')
+
+elif idade > 18:
+    print('Seu alistamento esta atrasado em {} anos.'.format(idade - 18))
+
+else:
+    print('falta {} anos para para seu alistamente.'.format(idade - 18))
